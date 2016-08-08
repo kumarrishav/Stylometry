b5c340efdd8c5cd9bd7d40eda6d684aa9551cf19
amrpandey <amrpandey@paypal.com>
2016-08-08 12:13:01 +0530

    Added paragrph class and updated reader class

---
+++ src/main/java/com/amriteya/stylometry/Paragraph.java
@@ -0,0 +1,37 @@
+package com.amriteya.stylometry;
+
+public class Paragraph {
+
+	
+	private Integer wordFrequency;
+	
+	private Integer punctuationFrequency;
+	
+	private Integer sentenceFrequency;
+
+	public Integer getWordFrequency() {
+		return wordFrequency;
+	}
+
+	public void setWordFrequency(Integer wordFrequency) {
+		this.wordFrequency = wordFrequency;
+	}
+
+	public Integer getPunctuationFrequency() {
+		return punctuationFrequency;
+	}
+
+	public void setPunctuationFrequency(Integer punctuationFrequency) {
+		this.punctuationFrequency = punctuationFrequency;
+	}
+
+	public Integer getSentenceFrequency() {
+		return sentenceFrequency;
+	}
+
+	public void setSentenceFrequency(Integer sentenceFrequency) {
+		this.sentenceFrequency = sentenceFrequency;
+	}
+	
+	
+}
--- src/main/java/com/amriteya/stylometry/Reader.java
+++ src/main/java/com/amriteya/stylometry/Reader.java
@@ -1,15 +1,96 @@
 package com.amriteya.stylometry;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.emory.mathcs.nlp.common.util.IOUtils;
+import edu.emory.mathcs.nlp.common.util.Joiner;
+import edu.emory.mathcs.nlp.component.tokenizer.EnglishTokenizer;
+import edu.emory.mathcs.nlp.component.tokenizer.Tokenizer;
+import edu.emory.mathcs.nlp.component.tokenizer.token.Token;
+
 public class Reader {
+	
+	
+	private static String PUNCTUATIONS = "[()’_?:!'\"“.,;{}]+";
 
 	public Author readAndUpdateBook(String filePath) {
 		// TODO Auto-generated method stub
 		
 		
+		BufferedReader br = IOUtils.createBufferedReader(filePath);
+		
+		try{
+			
+			String line;
+			
+			String paragraph = "";
+			List<Paragraph> paragraphList = new ArrayList<Paragraph>();
+			
+			while((line= br.readLine()) != null){
+				
+				paragraph += line + " ";
+				
+				if(line.trim().isEmpty()){
+					paragraphList.add(processParagraph(paragraph));
+					paragraph = "";
+				}
+					
+				
+			}
+			
+			paragraphList.add(processParagraph(paragraph));
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private Paragraph processParagraph(String paragraph) {
+		// TODO Auto-generated method stub
+		Paragraph p = new Paragraph();
+		
+		Tokenizer tokenizer = new EnglishTokenizer();
+		List<Token> list = tokenizer.tokenize(paragraph);
+		
+//		for(Token t: list)
+//			System.out.println(t);
+		p.setPunctuationFrequency(processPunctuationFrequency(list));
+		p.setSentenceFrequency(processSentenceFrequency(list));
+		removePunctuations(list);
+		p.setWordFrequency(processWordFrequence(list));
+		return p;
+	}
+	
+	private Integer processWordFrequence(List<Token> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Integer processSentenceFrequency(List<Token> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void removePunctuations(List<Token> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Integer processPunctuationFrequency(List<Token> list) {
+		// TODO Auto-generated method stub
 		
 		return null;
 	}
 
+	public static void main(String[] args){
+		Reader r = new Reader();
+		
+		r.readAndUpdateBook("src/resources/sample.txt");
+	}
 	
 	
 }
