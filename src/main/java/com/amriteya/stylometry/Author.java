efcfb1204bc1dba40b000ca90b363583156dff3f
amrpandey <amrpandey@paypal.com>
2016-08-08 11:30:19 +0530

    Added Author(book) details

--- src/main/java/com/amriteya/stylometry/Author.java
+++ src/main/java/com/amriteya/stylometry/Author.java
@@ -1,5 +1,60 @@
 package com.amriteya.stylometry;
 
+import java.util.ArrayList;
+
 public class Author {
 
+	
+	private String name;
+	
+	private ArrayList<String> wordFrequency;
+	
+	private Double averageWordsPerSentence;
+	
+	private Double punctuationDensity;
+	
+	private Double averageLinesPerParagraph;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<String> getWordFrequency() {
+		return wordFrequency;
+	}
+
+	public void setWordFrequency(ArrayList<String> wordFrequency) {
+		this.wordFrequency = wordFrequency;
+	}
+
+	public Double getAverageWordsPerSentence() {
+		return averageWordsPerSentence;
+	}
+
+	public void setAverageWordsPerSentence(Double averageWordsPerSentence) {
+		this.averageWordsPerSentence = averageWordsPerSentence;
+	}
+
+	public Double getPunctuationDensity() {
+		return punctuationDensity;
+	}
+
+	public void setPunctuationDensity(Double punctuationDensity) {
+		this.punctuationDensity = punctuationDensity;
+	}
+
+	public Double getAverageLinesPerParagraph() {
+		return averageLinesPerParagraph;
+	}
+
+	public void setAverageLinesPerParagraph(Double averageLinesPerParagraph) {
+		this.averageLinesPerParagraph = averageLinesPerParagraph;
+	}
+	
+	
+	
 }
